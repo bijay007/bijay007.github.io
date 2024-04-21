e0b55375b4fb5013326f7389c8228ac9e0ceaab3
Bijay Timilsina <bjtimi.007@gmail.com.com>
2024-04-21 21:48:51 +0200

    Final fix using eventlistners on pageload

--- src/app.js
+++ src/app.js
@@ -1,6 +1,37 @@
 function goHome() {
-    (document.getElementsByClassName('btn-home')[0]).addEventListener('click', function() {
-        window.location.href = '/';
-    });
+    const btnHome = document.getElementsByClassName('btn-home')[0];
+    if (btnHome) {
+        console.log(btnHome);
+        btnHome.addEventListener('click', function() {
+            window.location.href = '/';
+        });
+    }
 }
+function goToCategory() {
+    const btnCategory = document.getElementsByClassName('btn-category')[0];
+    if (btnCategory) {
+        console.log(btnCategory);
+        btnCategory.addEventListener('click', function() {
+            window.location.href = '/pages/category';
+        });
+    }
+}
+function goToDetails() {
+    const btnGoToDetails = document.getElementsByClassName('btn-details')[0];
+    const isHistoryPage = window.location.pathname.includes('history');
+    if (btnGoToDetails) {
+        console.log(btnGoToDetails);
+        btnGoToDetails.addEventListener('click', function() {
+            if (isHistoryPage) {
+                window.location.href = 'discography';
+            } else {
+                window.location.href = 'history';
+            }
+        });
+    }
+}
+
+// Add event-listeners on page load
 goHome();
+goToCategory();
+goToDetails();
